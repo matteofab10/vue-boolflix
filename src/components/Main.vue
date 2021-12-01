acd8d351d6c450ff11edac7d416cdc741e87650b
matteofab10 <matteofabio10@gmail.com>
2021-12-01 16:44:38 +0100

    fix grafici

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,14 @@
 <template>
   <main>
-    <div v-for="film in films" :key="film.id" class="container">
-      <ul>
-        <li>Titolo: {{film.title}}</li>
-        <li>Titolo originale: {{film.original_title}}</li>
-        <li>Lingua originale{{film.original_language}}</li>
-        <li>Voto: {{film.vote_average}}</li>
-      </ul>
+    <div class="container">
+      <div v-for="film in films" :key="film.id" class="film-card">
+        <ol>
+          <li>Titolo: {{film.title}}</li>
+          <li>Titolo originale: {{film.original_title}}</li>
+          <li>Lingua originale: {{film.original_language}}</li>
+          <li>Voto: {{film.vote_average}}</li>
+        </ol>
+      </div>
     </div>
   </main>
 </template>
@@ -34,14 +36,17 @@ export default {
     min-height: calc(100vh - 60px);
     background-color: grey;
     color: white;
+    overflow: auto;
     .container {
+      padding-top: 50px;
       display: flex;
       flex-wrap: wrap;
-      width: 300px;
-      padding-top: 50px;
     }
-    ul {
+    .film-card {
+      width: 350px;
+      background-color: red;
       color: white;
+      margin: 0 20px 10px 0;
     }
   }
 
